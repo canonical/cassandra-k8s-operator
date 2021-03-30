1e951dba4bc3490b65c9a0147cdc164303c3df96
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2021-03-30 02:41:02 +0000

    port change test

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -5,6 +5,7 @@ import unittest
 
 from ops.testing import Harness
 from charm import CassandraOperatorCharm
+from unittest.mock import patch
 
 
 class TestCharm(unittest.TestCase):
@@ -22,4 +23,20 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(
             self.harness.get_relation_data(rel_id, self.harness.model.app.name)["port"],
             "9042",
+        )
+
+    def test_port_change(self):
+        rel_id = self.harness.add_relation("cql", "otherapp")
+        self.assertIsInstance(rel_id, int)
+        self.harness.add_relation_unit(rel_id, "otherapp/0")
+        self.harness.update_relation_data(rel_id, "otherapp", {})
+        with patch.object(
+            CassandraOperatorCharm,
+            "goal_units",
+            return_value=self.harness.charm.num_units(),
+        ):
+            self.harness.update_config({"port": "9043"})
+        self.assertEqual(
+            self.harness.get_relation_data(rel_id, self.harness.model.app.name)["port"],
+            "9043",
         )
