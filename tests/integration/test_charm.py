be148066bb3ad1e9df56bd256978f2e13bacb293
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2021-08-20 14:15:20 +0000

    basic integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
 #  Copyright 2021 Canonical Ltd.
 #
@@ -14,8 +14,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-if [ "$1" == '--fix' ]; then
-    black src tests
-else
-    tox
-fi
+import pytest
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test):
+    my_charm = await ops_test.build_charm(".")
+    await ops_test.model.set_config({"update-status-hook-interval": "15s"})
+    await ops_test.model.deploy(
+        f"{my_charm.parents[0]}/cassandra-k8s_ubuntu-20.04-amd64.charm",
+        resources={
+            "cassandra-image": "cassandra:3.11",
+            "cassandra-prometheus-exporter": "cassandra-exporter-agent.jar",
+        },
+    )
+    await ops_test.model.wait_for_idle(wait_for_active=True)
