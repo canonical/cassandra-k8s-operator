f5f278ab2ee5a46d1e8eed6e6aff9524fd475f80
Ryan Barry <ryan.barry@canonical.com>
2021-09-29 23:10:33 +0000

    Final implementation

--- src/charm.py
+++ src/charm.py
@@ -157,10 +157,6 @@ class CassandraOperatorCharm(CharmBase):
         if isinstance(self.unit.status, BlockedStatus) and "dashboard" in self.unit.status.message:
             self.unit.status = ActiveStatus()
 
-        dashboard_tmpl = open(os.path.join(sys.path[0], "dashboard.json.tmpl"), "r").read()
-
-        self.dashboard_consumer.add_dashboard(dashboard_tmpl)
-
     def on_dashboard_broken(self, event):
         self.dashboard_consumer.remove_dashboard()
         if isinstance(self.unit.status, BlockedStatus) and "dashboard" in self.unit.status.message:
